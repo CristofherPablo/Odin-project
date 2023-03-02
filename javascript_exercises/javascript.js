ca0720b2faecedf43ab887289d6239f497225f01
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-01 21:00:24 -0300

    Starting on JavaScript

---
+++ javascript_exercises/javascript.js
@@ -0,0 +1,15 @@
+console.log(23+97);
+console.log(23+97+10+10+10+10);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+let max = 57;
+let actual = max-13;
+let percentage = actual/max;
+console.log(percentage);
+
+console.log(a =- 1);
