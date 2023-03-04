168a0a92fb36c5f64a6ce43ae1e93ea3f3e57b74
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-04 16:07:40 -0300

    Feat: trsnforme F to C and C to F functions

--- javaScript_Odin_Clone/07_tempConversion/tempConversion.js
+++ javaScript_Odin_Clone/07_tempConversion/tempConversion.js
@@ -1,11 +1,45 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  //C = (F - 32) * 5/9
+  let celsius = 0;
+  celsius = (fahrenheit - 32) * (5 / 9);
+
+  let howLong = celsius.toString();
+  howLong = howLong.split(".");
+  howLong = howLong[1];
+  if (howLong != undefined) {
+    howLong = howLong.length;
+    if (howLong > 1) {
+      return parseFloat(celsius.toFixed(1));
+    } else {
+      return celsius;
+    }
+  } else {
+    return celsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  //F = (C * 9/5) + 32
+  let fahrenheit = 0;
+  fahrenheit = celsius * (9 / 5) + 32;
+
+  let howLong = fahrenheit.toString();
+  howLong = howLong.split(".");
+  howLong = howLong[1];
+  if (howLong != undefined) {
+    howLong = howLong.length;
+    if (howLong > 1) {
+      return parseFloat(fahrenheit.toFixed(1));
+    } else {
+      return fahrenheit;
+    }
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
