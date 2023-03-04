df6f23452efdf0c5ea6eebd44c5060e07a165969
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-04 10:48:00 -0300

    Feat: function to repeat a string

--- javaScript_Odin_Clone/02_repeatString/repeatString.js
+++ javaScript_Odin_Clone/02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  var strConcat = "";
 
+  if (num < 0) {
+    return "ERROR";
+  } else if (str === "") {
+    return "";
+  } else if (num !== 0 && num !== null && str !== "") {
+    strConcat = str;
+    for (let i = 0; i < num - 1; i++) {
+      strConcat = strConcat + str;
+    }
+  }
+
+  return strConcat;
 };
 
 // Do not edit below this line
