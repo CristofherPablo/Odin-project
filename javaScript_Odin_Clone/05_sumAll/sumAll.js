04bf5397841d6ac4eed3a6abed5676d59704b705
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-04 14:38:46 -0300

    Feat: function to sum up all numbers between two numbers

--- javaScript_Odin_Clone/05_sumAll/sumAll.js
+++ javaScript_Odin_Clone/05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(number1, number2) {
+  
+    if(Number.isInteger(number1) && Number.isInteger(number2) 
+       && number1 >= 0 && number2 >= 0){
+        let largestNumber = 0;
+        let smallestNumber = 0;
+      if(number1 >= number2){
+        largestNumber = number1;
+        smallestNumber = number2;
+      }else{
+        largestNumber = number2;
+        smallestNumber = number1;
+      };
+      let sum = 0;
+      for(let i = smallestNumber; i <= largestNumber; i++){
+        sum += i;
+      };
+      return sum;
+    }else{
+      return 'ERROR';
+    };
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
