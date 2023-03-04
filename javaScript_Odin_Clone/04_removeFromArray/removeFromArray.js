a84c242e58c63ed64886c24e2edad3042d38f2d3
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-04 14:02:18 -0300

    Feat: function to remove an data from a string

--- javaScript_Odin_Clone/04_removeFromArray/removeFromArray.js
+++ javaScript_Odin_Clone/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  let arrayToUpdate = array.slice();
 
+  for (let y = 0; y < args.length; y++) {
+    for (let i = 0; i < arrayToUpdate.length; i++) {
+      if (arrayToUpdate[i] === args[y]) {
+        arrayToUpdate.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return arrayToUpdate;
 };
 
 // Do not edit below this line
