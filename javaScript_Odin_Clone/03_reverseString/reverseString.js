ffeeea030fc97ff6716b4d02241cd8ea59b015b4
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-04 11:18:44 -0300

    Feat: function to reverse a string

--- javaScript_Odin_Clone/03_reverseString/reverseString.js
+++ javaScript_Odin_Clone/03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var reverseStr = ''
+    if(str === ''){
+        return '';
+    }else if(str !== ''){
+      reverseStr = str[str.length - 1];
+      for (let i = str.length -2; i >= 0; i--){
+          reverseStr = reverseStr + str[i];
+      };
+    };
+    return reverseStr;
 };
 
 // Do not edit below this line
