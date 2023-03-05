ef78e0669b309134bf202dd3bd2a9fb968a1a061
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-05 14:42:50 -0300

    Feat: training DOM manipulation

---
+++ javascript_exercises/domManipulation.js
@@ -0,0 +1,29 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+container.appendChild(content);
+
+const para = document.createElement("p");
+para.textContent = "Hey I'm red!";
+para.style.color = "red";
+container.appendChild(para);
+
+const blueTitle = document.createElement("h3");
+blueTitle.textContent = "I’m a blue h3!";
+blueTitle.style.color = "blue";
+container.appendChild(blueTitle);
+
+const fatherDiv = document.createElement("div");
+//fatherDiv.setAttribute("id","father");
+
+const secondDiv = document.createElement("div");
+secondDiv.textContent = "I'm in a div";
+fatherDiv.appendChild(secondDiv);
+
+const paraDiv = document.createElement("p");
+paraDiv.textContent = "ME TOO!";
+fatherDiv.appendChild(paraDiv);
+
+container.appendChild(fatherDiv);
