d3e6584671e5ecf24dfc0f34a6921a845f8c4c8e
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-12 20:57:26 -0300

    Feat: Fibonacci number

--- javaScript_Odin_Clone/10_fibonacci/fibonacci.js
+++ javaScript_Odin_Clone/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 1){
+        return "OOPS";
+    }else if(num == 1 || num == 2){
+        return 1;
+    }else{
+        let aux = 0;
+        let ant1 = 1;
+        let ant2 = 1;
+        for(let i = 3; i <= num; i++){
+            aux = ant1;
+            ant1 = ant2;
+            ant2 = aux + ant1;
+        }
+        return ant2;
+    }
 };
 
 // Do not edit below this line
