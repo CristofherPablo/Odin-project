232ec6276e77a095280fb8564efec68e898fc0d0
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-12 20:33:18 -0300

    Feat: Palindrome function

--- javaScript_Odin_Clone/09_palindromes/palindromes.js
+++ javaScript_Odin_Clone/09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringOrigin = string.toLowerCase();
+    stringOrigin = stringOrigin.replace(/\W+/g, '');
+     let compare = "";
+     for(let i = stringOrigin.length - 1; i >= 0; i--){
+        compare += stringOrigin[i];
+     }
 
+     if(compare === stringOrigin){
+        return true;
+     }else{
+        return false;
+     }
 };
 
 // Do not edit below this line
