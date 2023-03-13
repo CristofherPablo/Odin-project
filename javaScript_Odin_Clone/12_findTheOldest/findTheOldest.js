78b346127c4f911594f756e947077d9137f9af09
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-12 21:44:31 -0300

    Feat: Find the oldest

--- javaScript_Odin_Clone/12_findTheOldest/findTheOldest.js
+++ javaScript_Odin_Clone/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let birth = 0;
+    let death = 0;
+    let antAge = 0;
+    let index = 0;
 
+    for(let i = 0; i < obj.length; i++){
+
+        birth = obj[i]['yearOfBirth'];
+        death = obj[i]['yearOfDeath'];
+
+        if(death === undefined){
+            death = 2023;
+        }
+
+        if((death - birth) >= antAge){
+            antAge = (death - birth);
+            index = i;
+        }
+    }
+    return obj[index];
 };
 
 // Do not edit below this line
