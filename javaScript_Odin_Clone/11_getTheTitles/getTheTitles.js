115db36a5df425403fb54c36c8c19e120eaef7ff
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-12 21:08:28 -0300

    Feat: function to search in an array of objects

--- javaScript_Odin_Clone/11_getTheTitles/getTheTitles.js
+++ javaScript_Odin_Clone/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let list = [];
+    let i = 0;
+    for(let name in books){
+        list[i] = books[name]['title'];
+        i++;
+    }
+    return list;
 };
 
 // Do not edit below this line
